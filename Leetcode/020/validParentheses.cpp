ee831447cf163998c25f8cc013d2d78a3877c97d
mengrenzi <mengrenzi1996@gmail.com>
2018-06-26 21:15:39 +0800

    add:leetcode(020)

---
+++ Leetcode/020/validParentheses.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stack>
+#include <cstdio>
+#include <cassert>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> stack;
+        for(int i = 0; i< s.size(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+                stack.push( s[i] );
+            else{
+                if(stack.size() == 0){
+                    return false;
+                }
+
+                char c = stack.top();
+                stack.pop();
+
+                char match;
+                if(s[i] == ')')
+                    match = '(';
+                else if( s[i] == ']')
+                    match = '[';
+                else{
+                    assert(s[i] == '}');
+                    match = '{';
+                }
+
+                if(c != match){
+                    return false;
+                }
+
+            }
+
+        }
+        if(stack.size() != 0)
+            return false;
+
+        return true;
+    }
+
+
+};
+
+
+int main(){
+
+    return 0;
+}
