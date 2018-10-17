abe48be56c385fcfc386670901c0b1e0297d8967
mengrenzi <mengrenzi1996@gmail.com>
2018-10-17 21:35:00 +0800

    add: Leetcode 059

--- LeetcodeJava/src/SpiralMatrix2.java
+++ LeetcodeJava/src/SpiralMatrix2.java
@@ -7,22 +7,36 @@ import java.util.Arrays;
  * @Copyright： 2018, Renzi Meng, All Rights Reserved.
  */
 
-class Solution {
+class Solution059 {
     public int[][] generateMatrix(int n) {
         int[][] results = new int[n][n];
         if(n == 0) return results;
-        int bot = n - 1, top = 0, left = n - 1, right = 0;
+        int bot = n - 1, top = 0, right = n - 1, left = 0, count = 1;
 
-        while(bot > top && )
 
-        while(bot >= top && right >= left){
-            for(int i = left; i< right; i++){result.add(matrix[top][i]);}
-            for(int i = top; i< bot; i++){result.add(matrix[i][right]);}
-            for(int i = right; i> left; i--){result.add(matrix[bot][i]);}
-            for(int i = bot; i> top; i--){result.add(matrix[i][left]);}
+        while(bot > top && right > left){
+            for(int i = left; i< right; i++){results[top][i] = count; count++;}
+            for(int i = top; i< bot; i++){results[i][right] = count; count++;}
+            for(int i = right; i> left; i--){results[bot][i] = count; count++;}
+            for(int i = bot; i> top; i--){results[i][left] = count; count++;}
             top++; left++; right--; bot--;
         }
 
+        if(right == left){
+            results[top][left] = count;
+        }
+
+        return results;
+    }
+
+    public static void main(String[] args) {
+        Solution059 mysolution = new Solution059 ();
+        int[][] x = mysolution.generateMatrix ( 3 );
+        for( int[] a : x){
+            for(int b : a){
+                System.out.println ( b );
+            }
+        }
     }
 }
 
