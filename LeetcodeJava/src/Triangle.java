de6ec0e53c1e26a0a794db33a7afd0aad08837eb
mengrenzi <mengrenzi1996@gmail.com>
2018-11-22 15:21:14 +0800

    add: leetcode 120

---
+++ LeetcodeJava/src/Triangle.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: Renzi Meng
+ * @Date: Created in 14:33 2018/11/22
+ * @Copyright： 2018, Renzi Meng, All Rights Reserved.
+ */
+
+public class Triangle {
+}
+
+class Solution120 {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle.size () == 0)   return 0;
+        int[] sumList = new int[triangle.size ()+1];
+
+        for(int i= triangle.size() - 1; i>= 0; i--) {
+            for(int j = 0; j< triangle.get ( i ).size(); j++) {
+                sumList[j] = Math.min(sumList[j],sumList[j+1])+triangle.get(i).get(j);
+            }
+        }
+
+        return sumList[0];
+    }
+}
+
+
